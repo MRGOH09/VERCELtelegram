4518d2ebe1c01cca94bb6ddfe232de4e8c9c3e4b
您的名字 <>
2025-08-12 14:09:17 +0800

    chore(api): remove deprecated endpoints to fit Hobby function limit

--- api/debug/echo.js
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-  return res.status(410).json({ ok: false, error: 'Deprecated debug endpoint' })
-}
-
--- api/leaderboard/today.js
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-  return res.status(410).json({ ok: false, error: 'Deprecated. Use /api/metrics?type=leaderboard' })
-}
-
